2931a96d6b5086377e49527945b4ae0da6b4e080
Sergio Acosta <sergioacostaquintana@gmail.com>
2026-02-15 23:12:28 +0000

    refactor(tests): simplify ErpApplicationTests structure

--- src/test/java/es/terencio/erp/ErpApplicationTests.java
+++ src/test/java/es/terencio/erp/ErpApplicationTests.java
@@ -1,31 +1,17 @@
 package es.terencio.erp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-@Testcontainers
-@ActiveProfiles("test")
-class ErpApplicationTests {
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
+/**
+ * Basic application context test.
+ * Extends AbstractIntegrationTest to use the shared PostgreSQL container.
+ */
+class ErpApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {
+        // This test ensures the Spring Context loads correctly
+        // using the shared container defined in AbstractIntegrationTest
     }
 
 }
